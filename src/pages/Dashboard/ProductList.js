2dfa5d3b12c2c9558abc4614bbc207a2bbcce6b0
ThakurSaad <thakursaad613@gmail.com>
2023-02-22 21:01:08 +0600

    testing

--- src/pages/Dashboard/ProductList.js
+++ src/pages/Dashboard/ProductList.js
@@ -41,7 +41,7 @@ const ProductList = () => {
 
             <tbody className="text-sm divide-y divide-gray-100">
               {products.map(({ model, brand, price, status, _id }) => (
-                <tr>
+                <tr key={_id}>
                   <td className="p-2">
                     <input type="checkbox" className="w-5 h-5" value="id-1" />
                   </td>
@@ -76,9 +76,9 @@ const ProductList = () => {
                           xmlns="http://www.w3.org/2000/svg"
                         >
                           <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
                             d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
                           ></path>
                         </svg>
