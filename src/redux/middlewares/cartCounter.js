59c6c26dfd2f4aea79ee4124fb9a01cca24c9452
ThakurSaad <thakursaad613@gmail.com>
2023-02-23 22:46:26 +0600

    Handle cartPosition with middleware

--- src/redux/middlewares/cartCounter.js
+++ src/redux/middlewares/cartCounter.js
@@ -1,7 +1,19 @@
+import { ADD_TO_CART } from "../actionTypes/actionTypes";
+
 const cartCounter = (store) => (next) => (action) => {
-  console.log("Current State", store.getState());
-  console.log("Action", action);
-  return;
+  const state = store.getState();
+  const cart = state.product.cart;
+
+  console.log(cart);
+
+  if (action.type === ADD_TO_CART) {
+    const newAction = {
+      ...action,
+      payload: { ...action.payload, cartPosition: cart.length },
+    };
+    return next(newAction);
+  }
+  return next(action);
 };
 
 export default cartCounter;
